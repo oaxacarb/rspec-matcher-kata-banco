98a98679998005b262ac800cd6d1875aae06b706
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2017-03-11 17:30:25 -0600

    Agrega visualizacion del numero erroneo.

--- lib/rspec_matcher_kata_banco.rb
+++ lib/rspec_matcher_kata_banco.rb
@@ -5,7 +5,14 @@ module RspecMatcherKataBanco
     index_errors = []
     match do |actual|
       expect.each_with_index do |e, i|
-        index_errors << i if e != actual[i]
+        unless e == actual[i]
+          h = {
+            index: i,
+            actual: actual[i],
+            expected: e
+          }
+          index_errors << h
+        end
       end
       index_errors.empty?
     end
@@ -13,8 +20,10 @@ module RspecMatcherKataBanco
     failure_message_for_should do |actual|
       formated_actual = format_array(actual)
       formated_expected = format_array(expected)
-      "expected\n#{formated_actual}\nto represent\n#{formated_expected}\n"\
-        "errors found in #{index_errors.join(', ')}"
+      "expected\n#{formated_actual}\nto represent\n#{formated_expected}\n" +
+      index_errors.map { |error|
+        "error found in index #{error[:index]} expected:\n#{format_simple_array(error[:expected])}\nactual:\n#{format_simple_array(error[:actual])}"
+      }.join("\n")
     end
 
     def format_array(v)
@@ -23,6 +32,10 @@ module RspecMatcherKataBanco
         v.map { |x| x[1] }.join,
         v.map { |x| x[2] }.join
       ].join("\n")
+    end
+
+    def format_simple_array(v)
+      v.join("\n")
     end
   end
 end
